7ead223a55cb75a3e037ff77b47867f0b8c6fbd5
hash19-git <harishsivan1806@gmail.com>
2024-03-10 10:23:23 +0530

    Add Python: added python File

---
+++ whatsapp.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+try:
+    pywhatkit.sendwhatmsg("+919944335320", "Hello from Hari", 13, 28)
+    print("Successfully Sent!")
+except ValueError as e:
+    print("An Unexpected Error!",e)
